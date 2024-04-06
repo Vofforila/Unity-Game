af43f5bf0be8237989fe4f71adca6a61d07d9792
Vofforila <polberci@yahoo.ro>
2024-04-06 19:35:25 +0300

    Update

--- Assets/_Scripts/Managers/CameraManager.cs
+++ Assets/_Scripts/Managers/CameraManager.cs
@@ -19,14 +19,14 @@ namespace Player
         private const int panAngleMax = 85;
 
         private Vector3 newPosition = Vector3.zero;
-        private float scroll;
         private int scrollArea = 25;
 
-        private float zoomDelta;
+        public Vector2 panLimit = Vector2.one * 40;
 
-        public void Update()
+        private void Update()
         {
-            newPosition = Vector3.zero;
+            newPosition = transform.position;
+
             // Camera Movement
             if (Input.GetKey(KeyCode.W) || Input.mousePosition.y > Screen.height - scrollArea)
             {
@@ -44,16 +44,47 @@ namespace Player
             {
                 newPosition = Vector3.right;
             }
-        }
 
-        public void FixedUpdate()
-        {
-            MoveCamera();
-        }
+            newPosition.x = Mathf.Clamp(newPosition.x, -panLimit.x, panLimit.x);
+            newPosition.z = Mathf.Clamp(newPosition.z, -panLimit.y, panLimit.y);
 
-        public void MoveCamera()
-        {
             transform.position += cameraSpeed * Time.deltaTime * newPosition;
         }
+
+        private void FixedUpdate()
+        {
+        }
+
+        /*   public void Update()
+           {
+               newPosition = Vector3.zero;
+               // Camera Movement
+               if (Input.GetKey(KeyCode.W) || Input.mousePosition.y > Screen.height - scrollArea)
+               {
+                   newPosition = Vector3.forward;
+               }
+               if (Input.GetKey(KeyCode.S) || Input.mousePosition.y < scrollArea)
+               {
+                   newPosition = Vector3.back;
+               }
+               if (Input.GetKey(KeyCode.A) || Input.mousePosition.x < scrollArea)
+               {
+                   newPosition = Vector3.left;
+               }
+               if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - scrollArea)
+               {
+                   newPosition = Vector3.right;
+               }
+           }
+
+           public void FixedUpdate()
+           {
+               MoveCamera();
+           }
+
+           public void MoveCamera()
+           {
+               transform.position += cameraSpeed * Time.deltaTime * newPosition;
+           }*/
     }
 }
